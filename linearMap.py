c3f0ecad44de6210cd043ba2926c69ec5a3b83c5
Joshan Bajaj <jbajaj1@jhu.edu>
2018-12-02 14:33:57 -0500

    Added more comments and moved translation to its own function

--- linearMap.py
+++ linearMap.py
@@ -2,22 +2,35 @@ import argparse
 import numpy as np
 from io import open
 
-#returns the matrices P and Q 
-#as well as the lists of corresponding words
+#Implementation of Linear Orthogonal Mapping of word embeddings
+#Much of this program is based on https://en.wikipedia.org/wiki/Kabsch_algorithm
+
+
+#Returns the matrices P and Q as well as
+#the lists of corresponding words. The format
+#of P and Q was based on the P and Q matrices
+#described in the "Description" section here
+#https://en.wikipedia.org/wiki/Kabsch_algorithm
+#However, our P and Q are Nx100 instead of Nx3
 def getMatrix(filename):
 	#LWords contains the ordering of the words
 	l1Words = []
 	l2Words = []
+
+	#Opens the data file. It is formatted as 
+	#SrcWord TgtWord SrcWordVec Delim TgtWordVec
 	rawdata = open(filename, 'r')
 
 	#LArrs contain lists of the 100 float vectors
 	l1Arr = []
 	l2Arr = []
 
-	#reads in input line by line to create LWords and creates
+	#Reads in input line by line to create LWords and creates
 	#the LArrs by creating each LVec point by point
 	for line in rawdata:
-
+		#Splits the line so that it's a list of the source word, the
+		#target word, the 100 dimensions of the source word vector, 
+		#the delimeter, then the 100 dimensions of the target word vector
 		line = line.split()
 
 		#Adds the words to L1Words and L2Words
@@ -26,9 +39,13 @@ def getMatrix(filename):
 
 		i = 2
 		delim = False
+
+		#The LVectors will contain the 100 dimensions of the word vector
 		l1Vec = []
 		l2Vec = []
 
+		#Iterates through the rest of the list to add each dimension 
+		#to its corresponding vector
 		while i < len(line):
 
 			#Error checks the line size
@@ -62,54 +79,88 @@ def getMatrix(filename):
 	l1M = np.vstack(l1Arr)
 	l2M = np.vstack(l2Arr)
 
-	#Centroids calculated
-	l1Cen = np.mean(l1M)
-	l2Cen = np.mean(l2M)
+	#Adjusts the matrix by subtracting 
+	#each point from the matrix's centroid
+	l1M = translation(l1M)
+	l2M = translation(l2M)
 
-	#Creating a 1x100 array to subtract each point in the
-	#LMatrices by their centroid
-	l1Subtract = np.full((1,100), l1Cen)
-	l2Subtract = np.full((1,100), l2Cen)
-
-	#Subtracts the centroid from each point
-	l1M = np.subtract(l1M, l1Subtract)
-	l2M = np.subtract(l2M, l2Subtract)
+	#Error checking
+	if len(l1Words) != len(l2Words):
+		raise ValueError(filename + " is formatted incorrectly!")
 
 	return (l1Words, l1M), (l2Words, l2M)
 
-#Computation of the covariance matrix
+#Translates the matrix L to make sure the centroid
+#is at the origin of the coordinate system
+#Found in the "Translation" section here
+#https://en.wikipedia.org/wiki/Kabsch_algorithm
+def translation(L):
+
+	#Calculates the centroid by getting the mean of 
+	#every point in the 2d matrix
+	centroid = np.mean(L)
+
+	#Creating a 1x100 array to subtract each point in the
+	#L matrix by the centroid
+	lSubtract = np.full((1, L.shape[1]), centroid)
+
+	#Subtracts the centroid from each point
+	L = np.subtract(L, lSubtract)
+
+	return L
+
+#Computates the covariance matrix
+#Found in the "Computation of the covariance matrix"
+#section here https://en.wikipedia.org/wiki/Kabsch_algorithm
 def getH(P, Q):
 	#P and Q is dimension Nx100
 	#pT is 100xN
 	#H is 100x100
 
+	#Finding P's transpose
 	pT = np.transpose(P)
+
+	#Calculating H by multiplying pT and Q
 	H = np.matmul(pT, Q)
 
 	return H
 
 #Computes optimal rotation matrix using SVD
+#Rotation algorithm found in "Computation of the optimal rotation matrix"
+#section here https://en.wikipedia.org/wiki/Kabsch_algorithm
 def rotation(H):
+	#Do SVD of H
 	U, S, vT = np.linalg.svd(H)
+
+	#Calculate V and U Transpose
 	V = np.transpose(vT)
 	uT = np.transpose(U)
 
-	#not sure what the det is used for yet
-	#from Wikipedia:
-	#decide whether we need to correct our rotation 
-	#matrix to ensure a right-handed coordinate system
+	#Calculate determinant of V uT 
 	d = np.linalg.det(np.matmul(V,uT))
-	#print(d)
+
+	#Create a diagonal matrix of 1s with the last point
+	#being the calculated determinant
 	Id = np.identity(V.shape[1], dtype=float)
 	Id[-1][-1] = d
+
+	#Calculate the rotation matrix V Id uT
 	R = np.matmul(np.matmul(V, Id), uT)
+
 	return R
 
+#Prints the results to an output file
 def returnRes(L1, L2, R, lang):
-	i = 0
+
+	#Creates an output file based on the name of the tgt lang
 	out = open(lang+'LO.txt', 'w')
-	for word in L1:
-		print(word, L2[i], *R[i], file=out)
+
+	#Iterates through L1 to print results
+	#L1, L2, and R3 have the same size, so i is used as a counter
+	i = 0
+	for _ in L1:
+		#Writes SrcWord TgtWord RotationVector to the output file
+		print(L1[i], L2[i], *R[i], file=out)
 		i += 1
 
 def main():
@@ -146,10 +197,13 @@ def main():
 	#Multiplies L1 by Rotation
 	rotL1Test = np.matmul(l1Test[1], rTrain)
 
+	#Error checks to make sure the rotated matrix has the right dimensions
+	if len(l1Test[0]) != rotL1Test.shape[0]:
+		raise ValueError("Rotated Matrix has incorrect dimensions!")
+
 	#Prints Results
 	returnRes(l1Test[0], l2Test[0], rotL1Test, args.tgt_lang)
 	
 if __name__ == '__main__':
     main()
-
 
