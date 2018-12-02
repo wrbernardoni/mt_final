3ce2f59fef8535f0641eb26323de054ff0ec9196
wrbernardoni <wrbernardoni@gmail.com>
2018-12-02 13:28:00 -0500

    Get Cos-Sim Variance

--- eval.cpp
+++ eval.cpp
@@ -23,6 +23,7 @@ int main(int argc, char** argv)
 
 
   long double totalSquaredError = 0.0;
+  long double totCos = 0.0;
   unsigned int count = 0;
   for (int i = 0; i < test.size() - 1; i++)
   {
@@ -34,16 +35,24 @@ int main(int argc, char** argv)
 
     count++;
     long double se = 0.0;
+    long double dot = 0.0;
+    long double an = 0.0;
+    long double bn = 0.0;
+
     for (int j = 0; j < test[i].tVec.size(); j++)
     {
       se += pow(test[i].tVec[j] - oVecs[test[i].s][j], 2);
+      dot += test[i].tVec[j] * oVecs[test[i].s][j];
+      an += test[i].tVec[j] * test[i].tVec[j];
+      bn += oVecs[test[i].s][j] * oVecs[test[i].s][j];
     }
     totalSquaredError += se;
+    totCos += dot / (sqrt(an) * sqrt(bn));
   }
 
   long double mse = totalSquaredError / count;
   long double totVa = 0.0;
-  long double totCos = 0.0;
+  long double cosVa = 0.0;
 
   for (int i = 0; i < test.size() - 1; i++)
   {
@@ -66,7 +75,8 @@ int main(int argc, char** argv)
     }
 
     va = pow(mse - se, 2);
-    totCos += dot / (sqrt(an) * sqrt(bn));
+    cosVa += pow(totCos / count - dot / (sqrt(an) * sqrt(bn)), 2);
+    
 
     cout << test[i].s << "->" << test[i].t << "--SE: " << se << " S.DEV: " << sqrt(va) << " COS: " << dot / (sqrt(an) * sqrt(bn)) << endl;
     totVa += va;
@@ -80,6 +90,7 @@ int main(int argc, char** argv)
   cout << "MEAN SQUARED ERROR: " << totalSquaredError / count << endl;
   cout << "STD DEVIATION: " << mVa << endl;
   cout << "MEAN COS SIM: " << totCos / count << endl;
+  cout << "COS SIM S.DEV: " << sqrt(cosVa / count) << endl;
   return 1;
 }
 
